94af3600669a22d71073f8644562b56d716f75ab
SAAD <s.meniar@edu.umi.ac.ma>
2024-10-30 23:04:04 +0100

    Add files via upload

--- build_c.py
+++ build_c.py
@@ -1,47 +1,47 @@
-from setuptools import setup, Extension
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
+import glob
+import torch
 
-# Define the correct directory path to the "groundingdino" sources
-current_dir = os.path.dirname(os.path.abspath(__file__))
-extensions_dir = os.path.join(current_dir, "GroundingSam", "groundingdino", "models", "GroundingDINO", "csrc")
+def get_extensions():
+    # Define the directory where the source files are located
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    extensions_dir = os.path.join(current_dir, "GroundingSam", "groundingdino", "models", "GroundingDINO", "csrc")
 
-# Source files for building
-main_source = os.path.join(extensions_dir, "vision.cpp")
-source_files = [
-    main_source,
-    os.path.join(extensions_dir, "MsDeformAttn", "ms_deform_attn_cpu.cpp"),
-    os.path.join(extensions_dir, "MsDeformAttn", "ms_deform_attn_cuda.cu")
-]
+    # Source files
+    main_source = os.path.join(extensions_dir, "vision.cpp")
+    source_files = glob.glob(os.path.join(extensions_dir, "**", "*.cpp")) + glob.glob(os.path.join(extensions_dir, "**", "*.cu"))
 
-# Check if CUDA is available
-use_cuda = os.getenv("CUDA_HOME") is not None
+    # Determine if CUDA is available
+    use_cuda = torch.cuda.is_available()
 
-# Configure the extension module
-extensions = [
-    CUDAExtension(
-        name="groundingdino._C",
-        sources=source_files,
-        include_dirs=[extensions_dir],
-        extra_compile_args={
-            'cxx': ['-O3'],
-            'nvcc': [
-                '-O3',
-                '-DCUDA_HAS_FP16=1',
-                '-D__CUDA_NO_HALF_OPERATORS__',
-                '-D__CUDA_NO_HALF_CONVERSIONS__',
-                '-D__CUDA_NO_HALF2_OPERATORS__',
-                '--expt-relaxed-constexpr'
-            ] if use_cuda else []
-        },
-        define_macros=[('WITH_CUDA', None)] if use_cuda else []
-    )
-]
+    # Define the extension
+    ext_modules = [
+        CUDAExtension(
+            name="groundingdino._C",
+            sources=[main_source] + source_files,
+            include_dirs=[extensions_dir],
+            extra_compile_args={
+                'cxx': ['-O3'],
+                'nvcc': [
+                    '-O3',
+                    '-DCUDA_HAS_FP16=1',
+                    '-D__CUDA_NO_HALF_OPERATORS__',
+                    '-D__CUDA_NO_HALF_CONVERSIONS__',
+                    '-D__CUDA_NO_HALF2_OPERATORS__',
+                    '--expt-relaxed-constexpr'
+                ] if use_cuda else []
+            },
+            define_macros=[('WITH_CUDA', None)] if use_cuda else []
+        )
+    ]
 
-# Setup for building
+    return ext_modules
+
 setup(
     name="groundingdino",
     version="0.1.0",
-    ext_modules=extensions,
+    ext_modules=get_extensions(),
     cmdclass={"build_ext": BuildExtension}
 )
